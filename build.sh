ade39be8fedcc4b86028eaa1c2877e20492ce02b
Chris Sandison <chris@thinkdataworks.com>
2024-03-05 13:52:38 -0500

    Including built jar in repo

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ echo "Copying trino-rabbitmq-event-listener output"
 
 id=$(docker create trino-rabbitmq-event-listener-builder)
 docker cp $id:/trino-rabbitmq-event-listener/build/libs/trino-rabbitmq-event-listener-435.jar ./build >/dev/null
-cp ./build/trino-rabbitmq-event-listener-435.jar ./dist
+cp ./build/trino-rabbitmq-event-listener-435.jar ./jars
 docker rm -v $id
